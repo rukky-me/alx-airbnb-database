c3df8a641190b48a0c7537254b73be8a3b632ef8
rukky-me <dnameisjoy@gmail.com>
2025-07-05 02:57:23 +0100

    using COUNT +  RANK window function

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,54 @@
+/*Get a list of all users along with how many bookings each one has made. 
+Include users even if they have made zero bookings, and show those with the most bookings at the top
+-LEFT JOIN 
+-    booking b ON u.user_id = b.user_id
+-Joins the booking table to the user table.
+-LEFT JOIN ensures that all users are included in the result, even if they have no bookings.
+-The join is made where the user_id in both tables match (b.user_id = u.user_id).
+-b is an alias for the booking table.
+
+-GROUP BY 
+-    u.user_id, u.first_name, u.last_name
+-Because we’re using an aggregation function (COUNT), we must group the results.
+-GROUP BY ensures the count is calculated per user.
+-Grouping by user_id, first_name, and last_name allows us to include user details along with the count.
+
+-ORDER BY 
+    total_bookings DESC;
+-Sorts the results by total_bookings in descending order.
+-So users with the most bookings appear first in the output.
+*/
+
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    "user" u
+LEFT JOIN 
+    booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name
+ORDER BY 
+    total_bookings DESC;
+
+
+--Rank properties based on total number of bookings
+--Uses: COUNT() + RANK() (window function)
+
+
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM 
+    property p
+LEFT JOIN 
+    booking b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name
+ORDER BY 
+    booking_rank;

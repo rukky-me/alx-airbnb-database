60c4f103fb44df5c5570555e876645a3eb1be991
rukky-me <dnameisjoy@gmail.com>
2025-07-05 03:22:49 +0100

    creating appropriate indexes for the columns

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,17 @@
+-- Indexes for user table
+CREATE INDEX idx_user_user_id ON "user"(user_id);
+CREATE INDEX idx_user_email ON "user"(email);
+CREATE INDEX idx_user_role ON "user"(role);
+
+-- Indexes for booking table
+CREATE INDEX idx_booking_booking_id ON booking(booking_id);
+CREATE INDEX idx_booking_user_id ON booking(user_id);
+CREATE INDEX idx_booking_property_id ON booking(property_id);
+CREATE INDEX idx_booking_status ON booking(status);
+CREATE INDEX idx_booking_created_at ON booking(created_at);
+
+-- Indexes for property table
+CREATE INDEX idx_property_property_id ON property(property_id);
+CREATE INDEX idx_property_host_id ON property(host_id);
+CREATE INDEX idx_property_location ON property(location);
+CREATE INDEX idx_property_pricepernight ON property(pricepernight);
